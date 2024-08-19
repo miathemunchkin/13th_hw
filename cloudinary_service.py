af82127327d668842afde097c61d93a28f4cf6a4
miathemunchkin <163035623+miathemunchkin@users.noreply.github.com>
2024-08-19 21:51:40 +0300

    Add files via upload

---
+++ cloudinary_service.py
@@ -0,0 +1,4 @@
+import cloudinary
+import cloudinary.uploader
+
+def upload_avatar_to_cloudinary(file):
---
+++ email_service.py
@@ -0,0 +1,3 @@
+import smtplib
+
+def send_verification_email(to_email: str, verification_link: str):
